47a2a6896a05861119429cfca12c1405c3c7ddb2
llenk <ellenkeal@gmail.com>
2017-09-28 13:12:28 -0500

    AddNumber

--- editboard.js
+++ editboard.js
@@ -1,16 +1,34 @@
+window.onload = function setTabIndicies() {
+  var index = 1;
+  for (var i=0; i<9; i++) {
+    for (var j=0; j<9; j++) {
+      document.getElementById("e"+i+j).setAttribute('tabindex', index);
+      index++;
+    }
+  }
+}
+
 function doSomething(e) {
     if (e.target !== e.currentTarget) {
         var clickedItem = e.target.childNodes[1];
+        clickedItem.focus();
         clickedItem.addEventListener('keydown', addNumber);
-
     }
     e.stopPropagation();
 }
 
 function addNumber(e) {
-  clickedCoords = [clickedItem.id[1], clickedItem.id[2]];
-  console.log(e.type);
+  var val = e.keyCode;
+  if (val>=49 && val<=57) {
+    val -= 48;
+    e.target.innerHTML = val;
+  }
 }
 
-var theParent = document.getElementById('gameboard');
-theParent.addEventListener("click", doSomething, false);
+function checkNumber(e) {
+
+}
+
+var board = document.getElementById('gameboard');
+board.addEventListener("click", doSomething, false);
+board.addEventListener("keydown", checkNumber);
