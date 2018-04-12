a7b5810576a2aa34ad4200de270338cf18b83355
llenk <ellenkeal@gmail.com>
2018-04-12 17:52:16 -0500

    does something semi-useful

---
+++ README.md
@@ -0,0 +1,17 @@
+# Sudoku
+
+A way to solve sudokus you've found elsewhere. Work in progress.
+
+## Currently
+
+You can input your problem by clicking the input button. The cells you've input then will show up green, so you can distinguish them from the cells you've added. 
+
+## Future
+
+* Check if cells could be correct, given what already exists
+* Not allow green cells to be changed outside of input mode
+* Have application solve the sudoku, and add "hint" button
+     * Add "guess and check" mode, where any wrong answer will be flagged, instead of just things that don't fit with current cells
+* Fix bug where anything can be clicked and made yellow
+* Better styling
+* Message when correctly solved
--- editboard.js
+++ editboard.js
@@ -1,4 +1,4 @@
-window.onload = function setTabIndicies() {
+function setTabIndicies() {
   let index = 1;
   for (let i=0; i<9; i++) {
     for (let j=0; j<9; j++) {
@@ -14,10 +14,10 @@ function pickBox(e) {
   if (e.target.className == 'd') {
     clickedItem = clickedItem.firstElementChild;
   }
-  if (previouslyClickedItem != clickedItem) {
+  if (previouslyClickedItem != clickedItem && previouslyClickedItem.parentElement.style.background != "lightgreen") {
     previouslyClickedItem.parentElement.style.background = 'white';
   }
-  clickedItem.parentElement.style.background = 'yellow';
+  clickedItem.parentElement.style.background = '#fffca7';
   clickedItem.focus();
   clickedItem.addEventListener('keydown', addNumber);
   e.stopPropagation();
@@ -28,20 +28,46 @@ function addNumber(e) {
   let val = e.keyCode;
   if (val>=49 && val<=57) {
     val -= 48;
-    e.target.innerHTML = val;
+    $(e.target).html(val);
+    if (!solving) {
+      let clickedItem = e.target;
+      if (e.target.className == 'd') {
+        clickedItem = clickedItem.firstElementChild;
+      }
+      clickedItem.parentElement.style.background = 'lightgreen';
+    }
   }
 }
 
-function checkNumber(e) {
-
+function inputProblem() {
+  solving = false;
 }
 
-let previouslyClickedItem;
+function solveProblem() {
+  solving = true;
+}
 
+// the below doesn't actually check if it's right
+// function solved(board) {
+//   let isSolvedBool = true;
+//   for (let i=0; i<9; i++) {
+//     for (let j=0; j<9; j++) {
+//       if ($('#e'+i+j).html() ==  "") {
+//         isSolvedBool = false;
+//       }
+//     }
+//   }
+//   return isSolvedBool;
+// }
+
+let previouslyClickedItem; // is there a way to do this without making this a global variable?
+let solving = true;
 $(document).ready( function() {
   let board = document.getElementById('gameboard');
+  setTabIndicies();
   previouslyClickedItem = document.getElementById('e00'); //just set an initial value here
   board.addEventListener("click", pickBox, true);
-  board.addEventListener("keydown", checkNumber);
+  $('#input').on('click', inputProblem);
+  $('#solve').on('click', solveProblem);
 });
 
